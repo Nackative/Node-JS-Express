db4cb1142e01ef82b35b0698520bd24d5be246dd
Nackative <53878504+Nackative@users.noreply.github.com>
2023-05-23 21:05:29 +0700

    Update app.js  products

--- app.js
+++ app.js
@@ -3,21 +3,30 @@ const chalk = require('chalk')
 const debug = require('debug')('app');
 const morgan = require('morgan');
 const path = require('path');
+const productsRouter = express.Router();
 
 const app = express();
 const PORT = process.env.PORT;
 
 app.use(morgan('combined'));
-app.use(express.static(path.join(__dirname,"/public/")));  //เรียกใช้ file index_16.html ใน path public/src/index_16.html
+app.use(express.static(path.join(__dirname, "/public/")));  //เรียกใช้ file index_16.html ใน path public/src/index_16.html
 
-app.set("views","./src/views");
+app.set("views", "./src/views");
 app.set("view engine", "ejs")
-app.get("/", (req, res) =>{
 
-    res.render('index', {username:'Sapphaya',customers:["Sapphaya", "Snacknoii", "Thailand"]});
+productsRouter.route("/").get((req, res) => {
+    res.send("Wellcome My Products");
+}
+);
 
-}) 
+app.use("/products", productsRouter)
 
-app.listen(PORT, ()=>{
+app.get("/", (req, res) => {
+
+    res.render('index', { username: 'Sapphaya', customers: ["Sapphaya", "Snacknoii", "Thailand"] });
+
+})
+
+app.listen(PORT, () => {
     debug("Listerning on port : " + chalk.yellow(PORT));
 })
