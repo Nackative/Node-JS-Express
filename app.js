d149655915e289e38d87b63353b6198f148a52dd
Nackative <53878504+Nackative@users.noreply.github.com>
2023-05-26 21:29:21 +0700

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.set("views", "./src/views");
 app.set("view engine", "ejs")
 
 productRouter.route("/").get((req, res) =>{
-     res.send("Wellcome My Products");
+     res.render("products");
 }
 );
 
